2ee886db5928fc72043931db91d0e1c758386aa5
Hactchubas <kauamaia@yahoo.com.br>
2023-05-22 23:56:52 -0300

    Comecei a adicionar a pesquisa e alguma formatação à pagina, foi necessário adicionar uma tabela, pois acho que deve ser discutido como o resultado dessa pesquisa vai aparecer

--- inventario_labs/index.php
+++ inventario_labs/index.php
@@ -1,10 +1,23 @@
+
+<?php
+    // require 'conexao.php';
+    
+    // if(isset($_POST['modelo'])){
+    //     $sql = $conexao->prepare("INSERT INTO tb_modelos VALUES(?,?,?,?,?,?,?,?)");
+    //     $sql->execute(array($_POST['modelo'],$_POST['lab1'],$_POST['lab2'],$_POST['lab3'],$_POST['lab4'],$_POST['lab5'],$_POST['lab6'],''));
+    //     echo 'Modelo inserido com sucesso';
+    //     header("location:modificar_inventario.php");
+    // }
+    
+?>
+
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="style.css" >
     <title>Início</title>
 </head>
 <body>
@@ -30,19 +43,53 @@
 
     <?php } else {  ?>
     
-    <h1> Bem vindo ao Inventário do SMD!</h1>
-    <h2>Selecione um laboratório</h2>
-    <a href="info_laboratorios.php?lab=1"><button>Laboratório 1</button></a>
-    <a href="info_laboratorios.php?lab=2"><button>Laboratório 2</button></a>
-    <a href="info_laboratorios.php?lab=3"><button>Laboratório 3</button></a>
-    <a href="info_laboratorios.php?lab=4"><button>Laboratório 4</button></a>
-    <a href="info_laboratorios.php?lab=5"><button>Laboratório 5</button></a>
-    <a href="info_laboratorios.php?lab=6"><button>Laboratório 6</button></a>
-    <a href="login.php"><button>login</button></a>;
+    <h1> Bem vindo ao Inventário do SMD!</h1>   
     
+    <section>
+        <h2>Selecione um laboratório</h2>
+        <article>
+            <a href="info_laboratorios.php?lab=1"><button>Laboratório 1</button></a>
+            <a href="info_laboratorios.php?lab=2"><button>Laboratório 2</button></a>
+            <a href="info_laboratorios.php?lab=3"><button>Laboratório 3</button></a>
+            <a href="info_laboratorios.php?lab=4"><button>Laboratório 4</button></a>
+            <a href="info_laboratorios.php?lab=5"><button>Laboratório 5</button></a>
+            <a href="info_laboratorios.php?lab=6"><button>Laboratório 6</button></a>
+        </article>
+        <br>
+        <a href="login.php"><button>login</button></a>
+    </section>
 
     <?php };?>
+    <br><hr>
+    
+    <?php
+    
+        // include('conexao.php');
 
+        // $pesquisa = $_GET['busca']; 
+        // $sql_code = "SELECT * from tb_modelos WHERE modelo LIKE '%$pesquisa%' ";
+
+    ?>
+
+    <form action="">
+        <input type="search" name="busca" placeholder="Pesquisar modelo">
+        <button type="submit">Buscar</button>
+    </form>    
+    
+    <table border="1">
+        <tr>
+            <th>Modelo</th>
+            <th>Lab1</th>
+            <th>Lab2</th>
+            <th>Lab3</th>
+            <th>Lab4</th>
+            <th>Lab5</th>
+            <th>Lab6</th>
+        </tr>
+        <tr>
+            <td colspan ="7">O resultado da pesquisa vem aqui</td>
+        </tr>
+    </table>
 
 </body>
 </html>

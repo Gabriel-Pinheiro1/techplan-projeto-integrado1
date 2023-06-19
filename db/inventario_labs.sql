720918d3c259fcefca483ba419da5d06dca0a2d7
Gabriel Pinheiro <joaogabrielbvp@gmail.com>
2023-06-19 00:33:41 -0300

    página de regras adicionada

--- db/inventario_labs.sql
+++ db/inventario_labs.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 18-Maio-2023 às 23:29
+-- Tempo de geração: 19-Jun-2023 às 05:30
 -- Versão do servidor: 10.4.24-MariaDB
 -- versão do PHP: 8.1.6
 
@@ -20,6 +20,36 @@ SET time_zone = "+00:00";
 --
 -- Banco de dados: `inventario_labs`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `tabela_softwares`
+--
+
+CREATE TABLE `tabela_softwares` (
+  `id` int(100) NOT NULL,
+  `software` varchar(100) NOT NULL,
+  `lab1` bit(1) NOT NULL,
+  `lab2` bit(1) NOT NULL,
+  `lab3` bit(1) NOT NULL,
+  `lab4` bit(1) NOT NULL,
+  `lab5` bit(1) NOT NULL,
+  `lab6` bit(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Extraindo dados da tabela `tabela_softwares`
+--
+
+INSERT INTO `tabela_softwares` (`id`, `software`, `lab1`, `lab2`, `lab3`, `lab4`, `lab5`, `lab6`) VALUES
+(1, 'Altium', b'0', b'0', b'1', b'0', b'1', b'1'),
+(2, 'Android Studio', b'1', b'0', b'1', b'1', b'0', b'0'),
+(3, 'Arduino', b'0', b'1', b'1', b'0', b'0', b'1'),
+(4, 'Blender', b'1', b'0', b'1', b'0', b'0', b'0'),
+(5, 'Gimp', b'1', b'1', b'1', b'0', b'1', b'0'),
+(6, 'Processing', b'1', b'1', b'0', b'1', b'0', b'1'),
+(7, 'Visual Studio Code', b'1', b'1', b'0', b'0', b'0', b'0');
 
 -- --------------------------------------------------------
 
@@ -70,6 +100,33 @@ INSERT INTO `tb_comp` (`id_comp`, `posicao`, `fila`, `patrimonio`, `modelo`, `ip
 -- --------------------------------------------------------
 
 --
+-- Estrutura da tabela `tb_equipamentos`
+--
+
+CREATE TABLE `tb_equipamentos` (
+  `id` int(11) NOT NULL,
+  `modelo` varchar(100) NOT NULL,
+  `patrimonio` varchar(50) NOT NULL,
+  `fabricante` varchar(50) NOT NULL,
+  `lab1` tinyint(1) NOT NULL,
+  `lab2` tinyint(1) NOT NULL,
+  `lab3` tinyint(1) NOT NULL,
+  `lab4` tinyint(1) NOT NULL,
+  `lab5` tinyint(1) NOT NULL,
+  `lab6` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Extraindo dados da tabela `tb_equipamentos`
+--
+
+INSERT INTO `tb_equipamentos` (`id`, `modelo`, `patrimonio`, `fabricante`, `lab1`, `lab2`, `lab3`, `lab4`, `lab5`, `lab6`) VALUES
+(1, 'projetor', '2885677', 'Epson', 1, 1, 0, 1, 1, 0),
+(2, 'Ar-condicionar', '2345677', 'Philco', 0, 1, 0, 0, 1, 0);
+
+-- --------------------------------------------------------
+
+--
 -- Estrutura da tabela `tb_info_modelos`
 --
 
@@ -98,7 +155,34 @@ CREATE TABLE `tb_info_modelos` (
 INSERT INTO `tb_info_modelos` (`fabricante`, `modelo`, `processador`, `cpu_mark`, `mem_capacidade`, `mem_tipo`, `disco1_capacidade`, `disco1_tipo`, `disco1_modelo`, `disco2_capacidade`, `disco2_tipo`, `disco2_modelo`, `so_nome`, `so_compilacao`, `id`) VALUES
 ('aaaaaa', 'cc', 'aa', 'aa', 'aa', 'aa', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 1),
 ('aa', 'aa', 'aa', 'aa', 'aa', 'aa', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 2),
-('bb', 'bb', 'bb', 'bb', 'bb', 'bb', 'bb', '', 'b', 'b', 'b', 'b', 'b', 'bb', 3);
+('Dell', 'Dell optilex 780', 'intel core 2 QuadCPU 2.8GHz', '229', '4gb', 'ddr3-10600', '244GB', 'SSD', 'ADATA SU630', '500GB', 'HDD', 'ST500', 'Windows 10 pro', '19004', 3);
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `tb_info_softwares`
+--
+
+CREATE TABLE `tb_info_softwares` (
+  `id` int(100) NOT NULL,
+  `software` varchar(100) NOT NULL,
+  `categoria` varchar(100) NOT NULL,
+  `licenca` varchar(100) NOT NULL,
+  `versao` varchar(100) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Extraindo dados da tabela `tb_info_softwares`
+--
+
+INSERT INTO `tb_info_softwares` (`id`, `software`, `categoria`, `licenca`, `versao`) VALUES
+(1, 'Altium', 'Design', 'gratuita', '2.2.1.6'),
+(2, 'Android Studio', 'IDE', 'gratuita', '2021.2.1'),
+(3, 'Arduino', 'DEV', 'gratuita', '1.8.19'),
+(4, 'Blender', 'Animação', 'gratuita', '3.3'),
+(5, 'Gimp', 'Mídia', 'open source', '2.10'),
+(6, 'Processing', 'IDE', 'gratuita', '3.5.4'),
+(7, 'Visual Studio Code', 'Editor', 'gratuita', '');
 
 -- --------------------------------------------------------
 
@@ -122,9 +206,12 @@ CREATE TABLE `tb_modelos` (
 --
 
 INSERT INTO `tb_modelos` (`modelo`, `lab1`, `lab2`, `lab3`, `lab4`, `lab5`, `lab6`, `id`) VALUES
-('imac', 28, 20, 0, 0, 18, 87, 1),
+('imac', 0, 20, 0, 0, 18, 87, 1),
 ('lenovo', 0, 25, 0, 34, 0, 0, 2),
-('sansung', 0, 0, 0, 0, 0, 0, 7);
+('sansung', 0, 0, 0, 0, 0, 0, 7),
+('Dell optilex 780', 17, 15, 15, 0, 0, 0, 16),
+('Positivo D610', 0, 0, 0, 0, 0, 0, 17),
+('imac 16,2', 25, 0, 0, 0, 0, 0, 18);
 
 -- --------------------------------------------------------
 
@@ -152,69 +239,15 @@ INSERT INTO `tb_modelos-bkp` (`modelo`, `lab1`, `lab2`, `lab3`, `lab4`, `lab5`, 
 ('lenovo', 0, 25, 0, 34, 0, 0, 2),
 ('dell', 14, 91, 0, 23, 0, 0, 3);
 
--- --------------------------------------------------------
-
---
--- Estrutura da tabela `tabela_softwares`
---
-
-CREATE TABLE `tb_info_softwares` (
-  `id` int(100) NOT NULL PRIMARY KEY AUTO_INCREMENT,
-  `software` varchar(100) NOT NULL,
-  `categoria` varchar(100) NOT NULL,
-  `licenca` varchar(100) NOT NULL,
-  `versao` varchar(100)
-  ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
-
---
--- Extraindo dados da tabela `tb_modelos-bkp`
---
-
-INSERT INTO `tb_info_softwares` (`software`, `categoria`, `licenca`, `versao`) VALUES
-  ('Altium','Design','gratuita','2.2.1.6'),
-  ('Android Studio','IDE','gratuita','2021.2.1'),
-  ('Arduino','DEV','gratuita','1.8.19'),
-  ('Blender','Animação','gratuita','3.3'),
-  ('Gimp','Mídia','open source','2.10'),
-  ('Processing','IDE','gratuita','3.5.4'),
-  ('Visual Studio Code','Editor','gratuita','');
-
-
-
---
--- Estrutura da tabela `tabela_softwares`
---
-
-CREATE TABLE `tabela_softwares` (
-  `id` int(100) NOT NULL PRIMARY KEY AUTO_INCREMENT,
-  `software` varchar(100) NOT NULL, 
-  `lab1` bit(1) NOT NULL,
-  `lab2` bit(1) NOT NULL,
-  `lab3` bit(1) NOT NULL,
-  `lab4` bit(1) NOT NULL,
-  `lab5` bit(1) NOT NULL,
-  `lab6` bit(1) NOT NULL
-  ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
-
---
--- Extraindo dados da tabela `tb_modelos-bkp`
---
-
-  INSERT INTO `tabela_softwares` (`software`, `lab1`, `lab2`, `lab3`, `lab4`, `lab5`, `lab6`) VALUES
-    ('Altium',0,0,0,0,1,1),
-    ('Android Studio',1,0,0,1,0,0),
-    ('Arduino',0,1,0,0,0,1),
-    ('Blender',0,0,0,0,0,0),
-    ('Gimp',0,1,1,0,1,0),
-    ('Processing',1,1,0,1,0,1),
-    ('Visual Studio Code',0,0,0,0,0,0);
-
-
 --
 -- Índices para tabelas despejadas
 --
+
+--
+-- Índices para tabela `tabela_softwares`
+--
+ALTER TABLE `tabela_softwares`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Índices para tabela `tb_cadastro`
@@ -229,9 +262,21 @@ ALTER TABLE `tb_comp`
   ADD PRIMARY KEY (`id_comp`);
 
 --
+-- Índices para tabela `tb_equipamentos`
+--
+ALTER TABLE `tb_equipamentos`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Índices para tabela `tb_info_modelos`
 --
 ALTER TABLE `tb_info_modelos`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Índices para tabela `tb_info_softwares`
+--
+ALTER TABLE `tb_info_softwares`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -251,6 +296,12 @@ ALTER TABLE `tb_modelos-bkp`
 --
 
 --
+-- AUTO_INCREMENT de tabela `tabela_softwares`
+--
+ALTER TABLE `tabela_softwares`
+  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
 -- AUTO_INCREMENT de tabela `tb_cadastro`
 --
 ALTER TABLE `tb_cadastro`
@@ -263,16 +314,28 @@ ALTER TABLE `tb_comp`
   MODIFY `id_comp` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
+-- AUTO_INCREMENT de tabela `tb_equipamentos`
+--
+ALTER TABLE `tb_equipamentos`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
 -- AUTO_INCREMENT de tabela `tb_info_modelos`
 --
 ALTER TABLE `tb_info_modelos`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
+-- AUTO_INCREMENT de tabela `tb_info_softwares`
+--
+ALTER TABLE `tb_info_softwares`
+  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
 -- AUTO_INCREMENT de tabela `tb_modelos`
 --
 ALTER TABLE `tb_modelos`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
 
 --
 -- AUTO_INCREMENT de tabela `tb_modelos-bkp`
